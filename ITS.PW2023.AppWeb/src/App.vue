0bbeb9591c682629b02e78833b63cb2acf8a20c1
chiaracicuta <ext-ccicuta@tempestive.com>
2023-04-28 15:12:47 +0200

    added login page without validation or redirect

--- ITS.PW2023.AppWeb/src/App.vue
+++ ITS.PW2023.AppWeb/src/App.vue
@@ -1,26 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <LoginPage></LoginPage>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LoginPage from './components/Login.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    LoginPage
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Rockwell;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        color: var(--color-p3-text);
+    }
 </style>
---
+++ ITS.PW2023.AppWeb/src/components/Login.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="app">
+        <div class="title">
+            <h1>NOME APP</h1>
+        </div>
+        <div class="loginForm">
+            <div class="input-group flex-nowrap loginField">
+                <span class="input-group-text icon" id="addon-wrapping"><i class="bi bi-person-fill"></i></span>
+                <input type="text" class="form-control font" placeholder="Username" aria-label="Username" aria-describedby="addon-wrapping">
+            </div>
+            <div class="input-group flex-nowrap loginField">
+                <span class="input-group-text icon" id="addon-wrapping"><i class="bi bi-key-fill"></i></span>
+                <input type="password" class="form-control font" placeholder="Password" aria-label="Password" aria-describedby="addon-wrapping">
+            </div>
+            <div class="col-12">
+                <button type="submit" class="btn loginButton" @click="validateCredentials">LOGIN</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+        name: "LoginPage",
+        methods: {
+            validateCredentials() {
+                /*this.reditectToHomePage();*/
+            },
+            redirectToHomePage() {
+                // redirect
+            }
+        }
+}
+</script>
+
+<style>
+    .app {
+        top: 50%;
+        position: absolute;
+        transform: translate(0, -50%);
+    }
+    .title {
+        text-align: center;
+        margin-bottom: 15px;
+    }
+    .loginForm {
+        margin: auto;
+    }
+    .loginField {
+        margin-bottom: 15px;
+        height: 40px;
+    }
+    .icon {
+        font-size: large;
+        color: var(--color-p3-text);
+    }
+    .font {
+        font-family: Helvetica;
+        color: var(--color-p3-text);
+    }
+    .loginButton {
+        border: 2px solid var(--color-p3-text);
+        color: var(--color-p3-text);
+        background-color: transparent;
+    }
+    .loginButton:hover {
+        border: 2px solid var(--color-p3-text);
+        color: var(--color-p3-bg);
+        background-color: var(--color-p3-text);
+    }
+</style>
